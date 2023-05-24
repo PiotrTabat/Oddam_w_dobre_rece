d3c6375bb7982b0dee9e42fb6c0e06ba0b188045
piotrtabat <piotr.tabat1@gmail.com>
2023-05-24 18:04:06 +0200

    refactor

--- oddam_w_dobre_rece/src/App.js
+++ oddam_w_dobre_rece/src/App.js
@@ -3,15 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Hero from "./components/Hero";
-import SimpleSteps from "./components/SimpleSteps";
-import AboutUs from "./components/AboutUs";
-import WhomWeHelp from "./components/WhomWeHelp";
-import Contact from "./components/Contact";
 import Logout from "./pages/Logout";
-
-
-
 
 const App = () => {
     return (
@@ -21,11 +13,6 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/logout" element={<Logout />} />
-                <Route path="/hero" element={<Hero />} />
-                <Route path="/simplesteps" element={<SimpleSteps />} />
-                <Route path="/aboutus" element={<AboutUs />} />
-                <Route path="/whomwehelp" element={<WhomWeHelp />} />
-                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
     );
